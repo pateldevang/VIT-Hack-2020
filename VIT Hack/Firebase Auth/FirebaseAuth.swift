97fcd184ed515f1966482b0671e231fe46b63648
pateldevang <devangpatel92@gmail.com>
2020-04-02 23:39:56 +0530

    Firebase email Auth boilerplate added.

---
+++ VIT Hack/Firebase Auth/FirebaseAuth.swift
@@ -0,0 +1,70 @@
+//
+//  FirebaseAuth.swift
+//  VIT Hack
+//
+//  Created by Devang Patel on 02/04/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+
+class FirebaseAuth: UIViewController {
+    
+    
+    //MARK: - Function for Login using email and password
+    public static func emailLoginIn(email: String, pass: String, completion: @escaping (String) -> ()) {
+        
+        Auth.auth().signIn(withEmail: email, password: pass) { (user, error)
+            in
+            if error != nil {
+                // Vibrates on errors
+                UIDevice.invalidVibrate()
+                completion(error?.localizedDescription ?? "Error")
+            }
+            else {
+                // Vibrates on valid
+                UIDevice.validVibrate()
+                completion("Sucess")
+            }
+        }
+    }
+    
+    
+    // MARK: - Function for Sign-In using email and password
+    public static func emailSignIn(email: String, pass: String, completion: @escaping (String) -> ()) {
+        
+        Auth.auth().createUser(withEmail: email, password: pass) { (authResult, error)
+            in
+            if error != nil {
+                // Vibrates on errors
+                UIDevice.invalidVibrate()
+                completion(error?.localizedDescription ?? "Error")
+            }
+            else {
+                // Vibrates on valid
+                UIDevice.validVibrate()
+                completion("Sucess")
+            }
+        }
+    }
+    
+    //MARK: - Function for forget password using registered Email-ID
+    public static func forgetPassword(email: String, completion: @escaping (String) -> ()) {
+        Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+            if error != nil {
+                // Vibrates on errors
+                UIDevice.invalidVibrate()
+                completion(error?.localizedDescription ?? "Error")
+            }
+            else {
+                // Vibrates on valid
+                UIDevice.validVibrate()
+                completion("Sucess")
+            }
+        }
+        
+    }
+    
+}
---
+++ VIT Hack/Firebase Auth/firebaseSignOut.swift
@@ -0,0 +1,32 @@
+//
+//  firebaseSignOut.swift
+//  VIT Hack
+//
+//  Created by Devang Patel on 02/04/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+extension UIViewController {
+    
+    //MARK: - Signout settings
+    func signOut() {
+        
+        //TODO: - Set it after getting design
+//        let firebaseAuth = Auth.auth()
+//        do {
+//            // Set initial user default for login as false
+//            UserDefaults.standard.set(false, forKey: "login")
+//            try firebaseAuth.signOut()
+//            print("SignOut sucessful")
+//            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//            let controller = storyboard.instantiateViewController(withIdentifier: "loginViewController")
+//            self.present(controller, animated: true, completion: nil)
+//        } catch let signOutError as NSError {
+//            print ("Error signing out: %@", signOutError)
+//        }
+    }
+    
+}

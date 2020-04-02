95ff9e7d602e6ab01e0bd2552c592d6157d7293e
pateldevang <devangpatel92@gmail.com>
2020-04-02 23:49:09 +0530

    firebase data manager added.

---
+++ VIT Hack/Firebase Data Manager/dataManager.swift
@@ -0,0 +1,14 @@
+//
+//  dataManager.swift
+//  VIT Hack
+//
+//  Created by Devang Patel on 02/04/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class dataManager {
+    //TODO: - Functions to fetch firebase Database
+}

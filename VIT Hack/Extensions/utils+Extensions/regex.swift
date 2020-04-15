9ea95f13a945f4b0b7d3fcf710c967f1cabe57f9
pateldevang <devangpatel92@gmail.com>
2020-04-15 19:35:43 +0530

    phone regex for email and phone number added.

---
+++ VIT Hack/Extensions/utils+Extensions/regex.swift
@@ -0,0 +1,26 @@
+//
+//  regex.swift
+//  VIT Hack
+//
+//  Created by Devang Patel on 15/04/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isValidContact: Bool {
+        let phoneNumberRegex = "^[6-9]\\d{9}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneNumberRegex)
+        let isValidPhone = phoneTest.evaluate(with: self)
+        return isValidPhone
+    }
+    var isEmail: Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}", options: .caseInsensitive)
+            return regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count)) != nil
+        } catch {
+            return false
+        }
+    }
+}

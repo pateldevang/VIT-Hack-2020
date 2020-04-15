b723dfbf3c473ada544372a01cf423f799a864ac
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-04-15 14:17:17 +0530

    storyboard basic structure complete (#1)
    * all ViewControllers added
    
    * Tabbar setup done
    
    * basic structure complete

---
+++ VIT Hack/Controller/TabBar Controller/AskFAQVC.swift
@@ -0,0 +1,30 @@
+//
+//  AskFAQVC.swift
+//  VIT Hack
+//
+//  Created by Aaryan Kothari on 15/04/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import UIKit
+
+class AskFAQVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

1dad27fd7b43965711fda02797e29ccce760332c
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-07-15 09:10:28 +0530

    Storyboard UI Setup complete. (#8)
    * storyboard reset
    
    * -rm assets
    
    * button assets added
    
    * Lato font added
    
    * HomeViewController UI setup complete ✅
    
    * UserFormViewController UI setup complete ✅
    
    * PhoneViewController UI setup complete ✅
    
    * LoginViewController UI setup complete ✅
    
    * ForgotPasswordViewController UI Setup complete ✅
    
    * LoginFLow UI Setup complete ✅
    
    * Timeline Tableview + cell + TableviewHeader added
    
    * Profile UI Setup complete ✅
    
    * Tracks basic setup done
    
    * Domains basic setup complete
    
    * Main.storyboard basic UI setup complete ✅

---
+++ VIT Hack/View/FAQTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FAQTableViewCell.swift
+//  VIT Hack
+//
+//  Created by Aaryan Kothari on 15/07/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import UIKit
+
+class FAQTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

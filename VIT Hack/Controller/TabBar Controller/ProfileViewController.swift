d72854b794551b18f7c6eb0ff026801103b0b51d
aaryankotharii <aaryan.kothari@gmail.com>
2020-07-15 23:49:04 +0530

    outlets made

--- VIT Hack/Controller/TabBar Controller/ProfileViewController.swift
+++ VIT Hack/Controller/TabBar Controller/ProfileViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var initials: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var institute: UILabel!
+    @IBOutlet weak var registration: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func logoutClicked(_ sender: UIButton) {
     }
-    */
-
 }

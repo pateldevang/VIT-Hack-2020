9550005fd745be74f60a406b7f27a6561d2f3f34
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-30 01:05:06 +0530

    TracksCell outlets made

--- VIT Hack/View/Cells/TracksCell.swift
+++ VIT Hack/View/Cells/TracksCell.swift
@@ -9,16 +9,15 @@
 import UIKit
 
 class TracksCell: UITableViewCell {
-
+    
+    
+    @IBOutlet weak var header: UILabel!
+    @IBOutlet weak var body: UITextView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }

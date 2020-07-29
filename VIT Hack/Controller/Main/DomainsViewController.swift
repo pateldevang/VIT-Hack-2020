2eb0f37de26b91cd79d6b3edf3078d348bfc07df
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-29 12:39:34 +0530

    Testing

--- VIT Hack/Controller/Main/DomainsViewController.swift
+++ VIT Hack/Controller/Main/DomainsViewController.swift
@@ -12,13 +12,15 @@ class DomainsViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var domains = [DomainData(title: "Machine Learning", body: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ", Image: #imageLiteral(resourceName: "carbon_watson-machine-learning"), color: #colorLiteral(red: 0.6318229437, green: 0.8350182176, blue: 1, alpha: 1), problemStatements: [])]
+    var domains = [DomainData(title: "Machine Learning", body: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ", Image: #imageLiteral(resourceName: "carbon_watson-machine-learning"), color: #colorLiteral(red: 0.6318229437, green: 0.8350182176, blue: 1, alpha: 1), problemStatements: []),
+    DomainData(title: "Cyber Security", body: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ", Image: #imageLiteral(resourceName: "carbon_watson-machine-learning"), color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1), problemStatements: []),
+    DomainData(title: "App Development", body: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ", Image: #imageLiteral(resourceName: "carbon_watson-machine-learning"), color: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), problemStatements: []),
+    DomainData(title: "Web Development", body: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis ", Image: #imageLiteral(resourceName: "carbon_watson-machine-learning"), color: #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1), problemStatements: [])]
     
     let domainCellIdentifier = "domaincell"
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 

52f08c3ae05430ccb315b8c133f63df4feb00769
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-07-29 12:25:45 +0530

    DomainData model added

--- VIT Hack/Controller/Main/DomainsViewController.swift
+++ VIT Hack/Controller/Main/DomainsViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class DomainsViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +18,18 @@ class DomainsViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+
+extension DomainsViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
 }
---
+++ VIT Hack/Model/DomainData.swift
@@ -0,0 +1,16 @@
+//
+//  DomainData.swift
+//  VIT Hack
+//
+//  Created by Aaryan Kothari on 29/07/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import UIKit
+
+struct DomainData {
+    var title : String
+    var body : String
+    var Image : UIImage //TODO
+    var problemStatements : [String]
+}

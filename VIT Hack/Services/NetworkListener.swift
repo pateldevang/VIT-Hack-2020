48359e26cfe075e0ef94e8b639076d1814ba428a
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-01 09:57:23 +0530

    stopListener func added

--- VIT Hack/Services/NetworkListener.swift
+++ VIT Hack/Services/NetworkListener.swift
@@ -9,41 +9,48 @@
 import Foundation
 
 class NetworkListner : NSObject {
-
-static  let shared = NetworkListner()
-
-var reachabilityStatus: Reachability.Connection = .none
-let reachability = Reachability()
-
-var isNetworkAvailable : Bool {
-    return reachabilityStatus != .none
-}
-
-
-func startListner() {
     
-    NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
-    do {
-        try reachability?.startNotifier()
-    } catch {
-        print("Unable to start notifier")
+    static  let shared = NetworkListner()
+    
+    var reachabilityStatus: Reachability.Connection = .none
+    var reachability = Reachability()
+    
+    var isNetworkAvailable : Bool {
+        return reachabilityStatus != .none
     }
-}
-
-@objc func reachabilityChanged(note: Notification) {
     
-    let reachability = note.object as! Reachability
     
-    switch reachability.connection {
-    case .wifi:
-        print("REACHABILITY: WIFI")
-        StatusBarMessage.show(with: "Wifi Connected", style: .success, duration: 2.0)
-    case .cellular:
-        print("REACHABILITY: CELLULAR")
-        StatusBarMessage.show(with: "Connected", style: .success, duration: 2.0)
-    case .none:
-        print("REACHABILITY: NONE")
-        StatusBarMessage.show(with: "Network unavailable", style: .error, duration: .infinity)
+    func startListner() {
+        NotificationCenter.default.addObserver(self, selector: #selector(reachabilityChanged(note:)), name: .reachabilityChanged, object: reachability)
+        do {
+            try reachability?.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
     }
- }
+    
+    func stopListener() {
+        print("Stop Listener")
+        reachability?.stopNotifier()
+        NotificationCenter.default.removeObserver(self, name: .reachabilityChanged, object: nil)
+        reachability = nil
+    }
+    
+    
+    @objc func reachabilityChanged(note: Notification) {
+        
+        let reachability = note.object as! Reachability
+        
+        switch reachability.connection {
+        case .wifi:
+            print("REACHABILITY: WIFI")
+            StatusBarMessage.show(with: "Wifi Connected", style: .success, duration: 2.0)
+        case .cellular:
+            print("REACHABILITY: CELLULAR")
+            StatusBarMessage.show(with: "Connected", style: .success, duration: 2.0)
+        case .none:
+            print("REACHABILITY: NONE")
+            StatusBarMessage.show(with: "Network unavailable", style: .error, duration: .infinity)
+        }
+    }
 }

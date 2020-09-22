308edc3fabe7ebbcda9070eb1b9be16c2bfcfd40
Aaryan Kothari <aaryan.kothari@gmail.com>
2020-09-22 14:12:13 +0530

    updates

---
+++ Pods/Target Support Files/GoogleDataTransport-iOS14.0/GoogleDataTransport-iOS14.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GoogleDataTransport_iOS14_0 : NSObject
+@end
+@implementation PodsDummy_GoogleDataTransport_iOS14_0
+@end
---
+++ Pods/Target Support Files/nanopb-iOS14.0/nanopb-iOS14.0-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_nanopb_iOS14_0 : NSObject
+@end
+@implementation PodsDummy_nanopb_iOS14_0
+@end

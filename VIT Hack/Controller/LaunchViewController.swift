cad9c14eea6dd3b3953d6f84d77154821283f527
Aaryan Kothari <53724307+aaryankotharii@users.noreply.github.com>
2020-09-25 14:45:07 +0530

    Bug fixes and improvements  (#70)
    * image corner radius bug
    
    * launchVC added
    
    * gif fixes
    
    * VC generic function
    
    * gif setup complefe
    
    * Dark mode major updates
    
    * speaker animations added
    
    * aboutUs button  minor bug fixes
    
    * dark mode fixes
    
    * idk

---
+++ VIT Hack/Controller/LaunchViewController.swift
@@ -0,0 +1,86 @@
+//
+//  LaunchViewController.swift
+//  VIT Hack
+//
+//  Created by Aaryan Kothari on 25/09/20.
+//  Copyright © 2020 VIT Hack. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+import FirebaseAuth
+import GoogleSignIn
+
+class LaunchViewController: UIViewController {
+    @IBOutlet weak var launchImage: UIImageView!
+    
+    var timer = Timer()
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        playVideo()
+    }
+    
+    private func playVideo() {
+        let dark = self.traitCollection.userInterfaceStyle == .dark
+        let rescource = dark ? "dark" : "light"
+        guard let path = Bundle.main.path(forResource: rescource, ofType:"mp4") else {
+            debugPrint("video.m4v not found")
+            return
+        }
+        let player = AVPlayer(url: URL(fileURLWithPath: path))
+        let playerLayer = AVPlayerLayer(player: player)
+        playerLayer.frame = self.view.bounds
+        self.view.layer.addSublayer(playerLayer)
+        player.play()
+        
+        let duration = player.currentItem?.asset.duration.seconds ?? 0.0
+        print("DURATION:",duration)
+        timer = Timer.scheduledTimer(withTimeInterval: duration + 1.0, repeats: false, block: { (timer) in
+             player.pause()
+             timer.invalidate()
+            self.setInitialViewController()
+         })
+    }
+    
+    //MARK: - Function setting up intial view controller
+    func setInitialViewController() {
+        
+        let loginstatus = UserDefaults.standard.bool(forKey: Keys.login)
+        let onboarded = Defaults.onbaorded()
+        
+        debugLog(message: "Login: \(loginstatus)")
+        debugLog(message: "Onboarded: \(onboarded)")
+        
+        if !onboarded {
+            let VC = mainStoryboard.instantiateViewController(withIdentifier: "onbaording") as! OnboardingViewController
+            setViewController(VC)
+        } else {
+            if loginstatus{
+                let VC = mainStoryboard.instantiateViewController(withIdentifier: "tabbar") as! UITabBarController
+                setViewController(VC)
+            } else {
+                logout()
+                let VC = mainStoryboard.instantiateViewController(withIdentifier: "LoginVC") as! UINavigationController
+                setViewController(VC)
+            }
+        }
+    }
+    
+    func setViewController<view : UIViewController>(_ vc : view){
+        vc.modalTransitionStyle = .flipHorizontal
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+    func logout(){
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            GIDSignIn.sharedInstance().signOut()
+            debugLog(message: "SignOut successful")
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+}
--- VIT Hack/View/Cells/Sponsors/SpeakersCell.swift
+++ VIT Hack/View/Cells/Sponsors/SpeakersCell.swift
@@ -16,17 +16,22 @@ class SpeakersCell: UICollectionViewCell {
     @IBOutlet weak var company: UILabel!
     @IBOutlet weak var join: UIButton!
     
+    var pulsatingLayer : CAShapeLayer!
     
     func setupCell(_ data : SpeakersData){
         name.text = data.name
         designation.text = data.designation
         company.text = data.company
         setImage(data)
+        join.setTitle("Watch Now", for: .normal)
         join.layer.cornerRadius = 4
+        initialSetup(data.startUnix, data.endUnix)
     }
     
     func setImage(_ data : SpeakersData){
         image.kf.indicatorType = .activity
+        
+        let processor = RoundCornerImageProcessor(radius: .heightFraction(0.5))
         
         if let imageUrl =  data.imageUrl ,let url = URL(string: imageUrl){
             image.kf.setImage(
@@ -34,8 +39,50 @@ class SpeakersCell: UICollectionViewCell {
                 options: [
                     .scaleFactor(UIScreen.main.scale),
                     .transition(.fade(1)),
-                    .cacheOriginalImage
+                    .cacheOriginalImage,
+                    .processor(processor)
             ])
+        }
+    }
+    
+    func initialSetup(_ startTime : Double?, _ endTime : Double?){
+        guard let sTime = startTime else { return }
+        guard let eTime = endTime else { return }
+        if Date(timeIntervalSince1970: sTime) < Date() && Date(timeIntervalSince1970: eTime) > Date() {
+            setLayer()
+            join.setTitle("Join Now", for: .normal)
+        }
+    }
+    
+    func setLayer(){
+        removeSubLayer()
+        //Pulsating Layer + animate pulsating layer
+        pulsatingLayer = CAShapeLayer()
+        let pathForPulsatingLayer = UIBezierPath(arcCenter: .zero, radius: 40, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi , clockwise: true)
+        pulsatingLayer.path = pathForPulsatingLayer.cgPath
+        pulsatingLayer.strokeColor=UIColor.clear.cgColor
+        pulsatingLayer.lineWidth=15
+        pulsatingLayer.lineCap=CAShapeLayerLineCap.round
+        pulsatingLayer.fillColor = #colorLiteral(red: 0, green: 0.4431372549, blue: 0.8039215686, alpha: 0.4)
+        pulsatingLayer.position = image.center
+        contentView.layer.insertSublayer(pulsatingLayer, below: image.layer)
+        
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.toValue = 1.5
+        animation.duration=1
+        animation.timingFunction=CAMediaTimingFunction(name:CAMediaTimingFunctionName.easeOut)
+        animation.autoreverses=true
+        animation.repeatCount = Float.infinity
+        pulsatingLayer.add(animation, forKey:"abcd")
+    }
+    
+    func removeSubLayer(){
+        if let sublayers = contentView.layer.sublayers{
+            for sublayer in sublayers{
+                if sublayer == pulsatingLayer{
+                    sublayer.removeFromSuperlayer()
+                }
+            }
         }
     }
     
